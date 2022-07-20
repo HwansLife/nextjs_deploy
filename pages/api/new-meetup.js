02318ce0e20939c50814d93a0c027693f99167da
윤용환 <sos26276@naver.com>
2022-07-21 01:24:50 +0900

    upload

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+        const data = req.body;
+
+
+        const client = await MongoClient.connect('mongodb+srv://sos2627:dudgks@cluster0.r98vf.mongodb.net/?retryWrites=true&w=majority');
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
+    }
+}
+
+export default handler;
